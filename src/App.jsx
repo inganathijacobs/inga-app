047825a84fe1c39b4c60ac18ba56808bff690089
Your Name <your.email@example.com>
2025-07-25 08:11:56 +0200

    colorgame file

--- src/App.jsx
+++ src/App.jsx
@@ -66,47 +66,7 @@ function Dislike() {
     </div>
   );
 }
-// State Changes -> Update UI (useState)
-//Typing event -> Listens-> OnChange, onInpt, onKeyPress
-// onChange -> Works with Text,Checkbox, Radio buttons ,etc
-
-//event.target -> Where event originate from
-// component is a function of color
-function ColorGame() {
-  const [color, setColor] = useState("");
-
-  const styles = {
-    background: color,
-    fontSize: "24px",
-  };
-  //Task 1.1 List these colors using ColorBox
-  const [colors, setColors] = useState(["pink", "orange", "purple"]);
-  // const addColor = (event) => {
-  //   event.preventDefault();
-  //   setColors([...colors, color]);
-  // };
-  return (
-    <div>
-      <div className=".color-form-container">
-        <input
-          style={styles}
-          onChange={(event) => setColor(event.target.value)}
-          type="text"
-          placeholder="Your fav color.."
-        />
-        {/* <h1>{color}</h1> */}
-        <button onClick={() => setColors([...colors, color])}>+ Add</button>
-      </div>
-      <div className="color-box-list-container">
-        {colors.map((color) => (
-          <ColorBox color={color} />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-function ColorBox({ color }) {
+export function ColorBox({ color }) {
   const styles = {
     background: color,
     height: "25px",
---
+++ src/ColorGame.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { ColorBox } from './App';
+
+// State Changes -> Update UI (useState)
+//Typing event -> Listens-> OnChange, onInpt, onKeyPress
+// onChange -> Works with Text,Checkbox, Radio buttons ,etc
+//event.target -> Where event originate from
+// component is a function of color
+function ColorGame() {
+  const [color, setColor] = useState("");
+
+  const styles = {
+    background: color,
+    fontSize: "24px",
+  };
+  //Task 1.1 List these colors using ColorBox
+  const [colors, setColors] = useState(["pink", "orange", "purple"]);
+  // const addColor = (event) => {
+  //   event.preventDefault();
+  //   setColors([...colors, color]);
+  // };
+  return (
+    <div>
+      <div className=".color-form-container">
+        <input
+          style={styles}
+          onChange={(event) => setColor(event.target.value)}
+          type="text"
+          placeholder="Your fav color.." />
+        {/* <h1>{color}</h1> */}
+        <button onClick={() => setColors([...colors, color])}>+ Add</button>
+      </div>
+      <div className="color-box-list-container">
+        {colors.map((color) => (
+          <ColorBox color={color} />
+        ))}
+      </div>
+    </div>
+  );
+}
